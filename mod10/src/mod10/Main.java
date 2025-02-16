0c94c464b20ad4ebe29ed0c30f492e2f5666596b
biel965 <bieleugenio2005@gmail.com>
2025-02-16 17:21:31 -0300

    test commit

---
+++ mod10/src/mod10/Main.java
@@ -0,0 +1,11 @@
+package mod10;
+
+public class Main {
+	public static void main(String args[]) {
+		double numero = 10;
+		System.out.println(numero);
+		
+		Double numero2 = (double) numero;
+		System.out.println(numero2);
+	}
+}
